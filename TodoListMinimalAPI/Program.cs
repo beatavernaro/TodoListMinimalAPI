c9c205dac9b4b55414c2aab7d49e0c39239a46a9
Bea Tavernaro <94928593+beatavernaro@users.noreply.github.com>
2023-03-14 23:11:44 -0300

    Organizando os endpoints fora do program.cs

--- TodoListMinimalAPI/Program.cs
+++ TodoListMinimalAPI/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using TodoListMinimalAPI.Data;
 using System.Linq;
-
+using TodoListMinimalAPI.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,56 +16,6 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-//app.MapGet("/", () => "Hello World!"); //rota e função que vai ser chamada
-app.MapGet("/all", (AppDbContext context) =>
-{
-    var todos = context.Todos.ToList();
-    return Results.Ok(todos);
-});
-
-app.MapGet("/getbystatus/{status}", (bool status, AppDbContext context) => //nomear melhor rotas
-{
-    //var selectTodo = context.Todos.Find(status); // porque não funciona?
-    var selectTodo = (from s in context.Todos
-                      where s.Done == status
-                      select s);
-        
-    if(selectTodo is null) return Results.NotFound();
-    return Results.Ok(selectTodo);
-    
-});
-
-app.MapPost("/post", (AppDbContext context, Todo todo) => 
-{
-    context.Todos.Add(todo);
-    context.SaveChanges();
-    return Results.Created($"/post/{todo.Id}", todo);
-});
-
-app.MapPut("/put/{id}", (AppDbContext context, Todo todo, int id) =>
-{
-    var update = context.Todos.Find(id);
-    
-    if(update is null) return Results.NotFound();
-    
-    update.Title = todo.Title;
-    update.Done = todo.Done;
-    context.SaveChanges();
-    return Results.Ok(todo);
-    
-        
-});
-
-app.MapDelete("/del/{id}",(int id, AppDbContext context) =>
-{
-    var deleteTodo = context.Todos.Find(id);
-
-    if (deleteTodo is null) return Results.NotFound();
-    
-    var removed = context.Todos.Remove(deleteTodo);
-    context.SaveChanges();
-    return Results.Ok();  
-    
-});
+TodoListEndpointConfig.AddEndpoint(app);
 
 app.Run();
